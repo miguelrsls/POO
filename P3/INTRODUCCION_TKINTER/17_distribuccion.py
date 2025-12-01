8b19e801f3a5a0f6794872c4712863e82d24c39e
Miguel Rosales <miguel_3041240390@utd.edu.mx>
2025-12-01 17:39:56 -0600

    Delete P3/INTRODUCCION_TKINTER directory

--- P3/INTRODUCCION_TKINTER/17_distribuccion.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-ventana=Tk()
-ventana.title("Distribuccion")
-ventana.geometry("500x500")
-ventana.resizable(False, False)
--- P3/INTRODUCCION_TKINTER/8_text.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-ventana=Tk()
-ventana.title("Text")
-ventana.geometry("500x500")
-ventana.resizable(False, False)
